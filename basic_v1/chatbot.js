1e60dadd936fb98121df62265e517646e7d15689
Kur19 <kurtis.hassinger@gmail.com>
2018-11-27 04:44:23 -0700

    added html chatbot code
    added rivescript library and html code with reference to jquery from coding train #79 - Dan Shiffman

---
+++ basic_v1/chatbot.js
@@ -0,0 +1,18 @@
+// Chatbot's Brain
+! version = 2.0
+
+! sub who's = who is
+
++ *
+- try saying knock knock
+
++ [*] knock knock [*]
+- Who's there?
+
++ *
+% who is there
+- <star1> Who
+
++ *
+% * who
+- hahahahahahah
--- v2_rivescript/sketch.js
+++ v2_rivescript/sketch.js
@@ -1,18 +1,42 @@
-
-
 function setup() {
   noCanvas();
-  let lang = navigator.language || 'en-US';
-  let speechRec = new p5.SpeechRec(lang, gotSpeech);
-
+  let speech = new p5.Speech();
+  let speechRec = new p5.SpeechRec('en-US', gotSpeech);
   let continuous = true;
-  let interim = true;
+  let interim = false;
   speechRec.start(continuous, interim);
 
-  function gotSpeech() {
-    if (speechRec.resultValue) {
-      createP(speechRec.resultString);
-    }
-  }
+  let bot = new RiveScript();
 
+  bot.loadfile("brain.rive", brainReady, brainError);
+
+  function brainReady() {
+    console.log('Chatbot ready!');
+    bot.sortReplies();
+  }
+function brainError() {
+    console.log('Chatbot error!')
+}
+let button = select('#submit');
+let user_input = select('#user_input');
+let output = select('#output');
+
+button.mousePressed(chat);
+
+function gotSpeech() {
+ if (speechRec.resultValue) {
+   let input = speechRec.resultString;
+   user_input.value(input);
+   let reply = bot.reply("local-user", input);
+   speech.speak(reply);
+   output.html(reply);
+ }
+}
+
+
+
+function chat() {
+  let input = user_input.value();
+
+}
 }
