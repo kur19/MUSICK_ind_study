39c331728235b2ebd49c4d37ae810743d46a2161
Michael Musick <m.a.musick@gmail.com>
2019-03-05 13:26:46 -0700

    forced listening to others

--- v2_rivescript/sketch.js
+++ v2_rivescript/sketch.js
@@ -1,55 +1,78 @@
+/*jshint esversion: 6 */
+
 let der;
 
+
+
 function setup() {
-  noCanvas();
-  console.log("in standby mode, click mouse to start system!");
+    noCanvas();
+    console.log("in standby mode, click mouse to start system!");
 }
 
-function mousePressed(){
-  speechSetup();
+function mousePressed() {
+    speechSetup();
 }
 
-function speechSetup(){
-  let speech = new p5.Speech();
-  let speechRec = new p5.SpeechRec('en-US', gotSpeech);
-  let continuous = true;
-  let interim = false;
-  speechRec.start(continuous, interim);
+function speechSetup() {
+    let speech = new p5.Speech();
+    let speechRec = new p5.SpeechRec('en-US', gotSpeech);
+    let continuous = false;
+    let interim = false;
+    // speechRec.start(continuous, interim);
+    speechRec.continuous = continuous;
+    speechRec.interim = interim;
 
-  let bot = new RiveScript();
+    // start the listener
+    speechRec.start();
 
-  bot.loadFile("brain.rive").then(brainReady).catch(brainError);
+    // function to execute when speaking starts
+    speech.onStart = function() {
+        // console.log("started...");
+    };
 
-  function brainReady() {
-    console.log('Chatbot ready!');
-    bot.sortReplies();
-  }
-  function brainError() {
-    console.log('Chatbot error!')
-  }
-  //let button = select('#submit');
-  let user_input = select('#user_input');
-  let output = select('#output');
+    // function to execute when speaking stops
+    speech.onEnd = function() {
+        // console.log("stopped talking...");
+        // restart listener
+        speechRec.start();
+    };
 
-  //button.mousePressed(chat);
 
-  function gotSpeech() {
-    if (speechRec.resultValue) {
-      let input = speechRec.resultString;
-      console.log(input);
-      //user_input.value(input);
-      bot.reply("local-user", input).then(function(reply) {
-        speech.speak(reply);
-        // output.html(reply);
-        console.log(reply);
 
-      });
+    let bot = new RiveScript();
+
+    bot.loadFile("brain.rive").then(brainReady).catch(brainError);
+
+    function brainReady() {
+        console.log('Chatbot ready to play!');
+        bot.sortReplies();
     }
-  }
+    function brainError() {
+        console.log('Chatbot error!');
+    }
+    //let button = select('#submit');
+    let user_input = select('#user_input');
+    let output = select('#output');
+
+    //button.mousePressed(chat);
+
+    function gotSpeech() {
+        if (speechRec.resultValue) {
+            let input = speechRec.resultString;
+            console.log(input);
+            //user_input.value(input);
+            bot.reply("local-user", input).then(function(reply) {
+                speech.speak(reply);
+                // output.html(reply);
+                console.log(reply);
+
+            });
+        }
+    }
 
 
-  //function chat() {
-  //let input = user_input.value();
+    //function chat() {
+    //let input = user_input.value();
 
-  //}
+    //}
 }
