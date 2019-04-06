6eaa4a2e0817acc8d0c07d4a4561b43aa49f0003
Kurtis Hassinger <35817234+kur19@users.noreply.github.com>
2019-04-06 16:00:07 -0600

    Update sketch.js
    Proofed code in sketch.js file on both machines to make sure they matched.  Added missing "let" kickoff statements to file on secondary machine.

--- v2_rivescript/sketch.js
+++ v2_rivescript/sketch.js
@@ -30,6 +30,7 @@ function mousePressed() {
 
 function speechSetup() {
     let speech = new p5.Speech();
+    let kickoffSpeech = new p5.Speech();
     let listener = new p5.SpeechRec('en-US', gotSpeech);
     let continuous = false;
 
@@ -97,15 +98,26 @@ function speechSetup() {
 
     //button.mousePressed(chat);
 
+    let silenceCheckTimer;
+
     function gotSpeech() {
         if (listener.resultValue) {
             let input = listener.resultString;
             console.log(input);
             //user_input.value(input);
             bot.reply("local-user", input).then(function(reply) {
+                listener.rec.abort();
                 speech.speak(reply);
                 // output.html(reply);
                 console.log(reply);
+                clearTimeout(silenceCheckTimer);
+                silenceCheckTimer = setTimout( function(){}
+                    listener.rec.abort();
+                    agent.state = "speaking";
+                    kickoffSpeech.speak(kickoffStatements[2]);
+                  },
+                  8000
+                );
 
             });
         }
@@ -121,7 +133,7 @@ function speechSetup() {
             startListener();
         }
     }
-    setInterval(checkAgentState, 1000);
+    let agentCheckTimer = setInterval(checkAgentState, 1000);
 
 
     // START THE PROCESS!!!!
@@ -136,3 +148,13 @@ function speechSetup() {
 
     der = listener;
 }
+
+
+
+
+
+let kickoffStatements = [
+  "where did you go?"
+  "What is the meaning of life?"
+  "I'm listening."
+];
